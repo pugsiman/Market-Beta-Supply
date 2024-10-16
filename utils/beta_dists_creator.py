a500aff46b5f005f68684a3c189f63a4b254349d
Aviv Levinsky <levinskyaviv@gmail.com>
2024-10-16 16:02:15 -0700

    change tickers source to fetch externally

--- utils/beta_dists_creator.py
+++ utils/beta_dists_creator.py
@@ -14,15 +14,21 @@ def create_beta_distribution(sample_returns, date_str, tickers):
 
     if not os.path.exists(filepath):
         with open(filepath, 'w+') as f:
-            trailing_window = sample_returns.loc[date_str - pd.DateOffset(days=252) : date_str]
+            trailing_window = sample_returns.loc[
+                date_str - pd.DateOffset(days=252) : date_str
+            ]
 
-            welch_betas = []
+            welch_betas = {}
             for ticker in tickers.split(' '):
                 try:
-                    beta = Beta(trailing_window[BENCHMARK_INDEX], trailing_window[ticker]).welch()
-                    welch_betas.append(beta)
+                    beta = Beta(
+                        trailing_window[BENCHMARK_INDEX], trailing_window[ticker]
+                    ).welch()
+                    welch_betas[ticker] = beta
                 except KeyError:
-                    print(f'{ticker} ({date_str}) was truncated out of dataframe and could not be calculated')
+                    print(
+                        f'{ticker} ({date_str}) was truncated out of dataframe and could not be calculated'
+                    )
                     continue
 
             welch_series = pd.Series(welch_betas)
@@ -39,25 +45,37 @@ def persist_tickers(tickers):
 
     return filepath
 
-def main():
-    stocks_filepath = 'data/nasdaq_screener.csv'
-    df = pd.read_csv(stocks_filepath)
 
-    # filter out nanocap, biotech, warrants and other special instruments
-    sample_stocks = df[(df['Country'] == 'United States') & (df['Market Cap'] > 1000000) & (df['Sector'] != 'Health Care') & (df['Symbol'].astype(str).map(len) <= 4)]
+def main():
+    stocks_filepath = 'ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqtraded.txt'
+    df = pd.read_csv(stocks_filepath, sep='|')
+    sample_stocks = df[
+        (df['Test Issue'] == 'N')
+        & (df['ETF'] == 'N')
+        & (df['Symbol'].str.match(r'[A-Za-z]{2,4}'))
+        & ~(df['Security Name'].str.contains('ETN', na=False))
+    ]
     tickers = sample_stocks['Symbol'].values
     tickers_filepath = persist_tickers(tickers)
     tickers = open(tickers_filepath, 'r').read()
 
-    sample_data = Ticker(f'{tickers} {BENCHMARK_INDEX}', asynchronous=True).history(period='3y', interval='1d')['adjclose']
-    sample_returns = sample_data.unstack().T.pct_change(fill_method=None).dropna(axis='index', how='all')
+    sample_data = Ticker(f'{tickers} {BENCHMARK_INDEX}', asynchronous=True).history(
+        period='3y', interval='1d'
+    )['adjclose']
+
+    sample_returns = (
+        sample_data.unstack()
+        .T.pct_change(fill_method=None)
+        .dropna(axis='index', how='all')
+    )
     sample_returns.index = pd.DatetimeIndex(sample_returns.index).tz_localize(None)
-    dates = pd.bdate_range(start='1/1/2021', end=pd.to_datetime('now').tz_localize('EST').date())
-    
+    dates = pd.bdate_range(
+        start='1/1/2021', end=pd.to_datetime('now').tz_localize('EST').date()
+    )
+
     for date_str in dates:
         create_beta_distribution(sample_returns, date_str, tickers)
 
 
 if __name__ == '__main__':
     main()
-
