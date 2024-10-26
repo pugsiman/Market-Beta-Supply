1c3f4d4be51373df5f520fbc91f18b002a607b4a
Aviv Levinsky <levinskyaviv@gmail.com>
2024-10-25 22:02:58 -0700

    more cleanup

--- bwsa_beta_supply/main.py
+++ bwsa_beta_supply/main.py
@@ -19,54 +19,8 @@ from utils.beta import Beta
 pd.options.plotting.backend = 'plotly'
 
 BENCHMARK_INDEX = 'SPY'
-STOCK = ''
-JUNK_STOCKS = 'IONQ BBAI BTBT OPEN JOBY AFRM BYND SHOT NEWT BEEM APLD AAOI'
 start_date = '2018-01-01'
 today_date = datetime.today().strftime('%Y-%m-%d')
-
-
-def rolling_beta_fig(sample_returns):
-    cov = sample_returns.rolling(21).cov().unstack()[BENCHMARK_INDEX][STOCK]
-    var = sample_returns[BENCHMARK_INDEX].to_frame().rolling(21).var()
-    rolling_ols_beta = cov / var.iloc[:, 0]
-
-    sample_returns[STOCK] *= 100
-    sample_returns[BENCHMARK_INDEX] *= 100
-
-    returns_fig = sample_returns.plot().update_traces(opacity=0.3)
-    beta_fig = go.Figure(
-        data=go.Scatter(
-            x=rolling_ols_beta.index,
-            y=rolling_ols_beta.values,
-            name='beta',
-        )
-    ).update_traces(marker=dict(color='blue'))
-
-    fig = go.Figure(data=returns_fig.data + beta_fig.data).update_layout(
-        title=f'{STOCK} rolling beta', title_x=0.5
-    )
-    fig.show()
-
-
-def period_betas_window(chunk):
-    date_idx, df = chunk
-    beta = Beta(df[BENCHMARK_INDEX], df[STOCK])
-    trailing_window = sample_returns.loc[date_idx - pd.DateOffset(days=252) : date_idx]
-    inception_window = sample_returns.loc[:date_idx]
-
-    trailing_beta = Beta(trailing_window[BENCHMARK_INDEX], trailing_window[STOCK])
-    inception_beta = Beta(inception_window[BENCHMARK_INDEX], inception_window[STOCK])
-
-    return [
-        date_idx,
-        {
-            'ols_beta': beta.ols(),
-            'bsw_beta': beta.welch(rho=False),
-            'bswa_beta': beta.welch(),
-            'true_bswa_beta(inception)': inception_beta.welch(),
-            'true_bswa_beta(trailing)': trailing_beta.welch(),
-        },
-    ]
 
 
 def main():
